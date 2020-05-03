9f8955302a9af9abd1e5d0866c020a1a067cc921
Aaron Bull Schaefer <aaron@elasticdog.com>
2020-05-03 16:08:35 -0700

    Detect Rust files by hard-coded glob
    This is super rudimentary, but the basic functionality works.

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,12 @@
 
 use std::{
     error::Error,
+    ffi::OsStr,
     io::{self, Write},
+    path::{Path, PathBuf},
 };
 
+use glob::Pattern;
 use log::debug;
 
 pub mod config;
@@ -18,15 +21,34 @@ use crate::config::Config;
 pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
     debug!("'config': {:?}", config);
 
+    // TODO: convert directories into recursive lists of files
+    let files = config.paths().iter().filter(|f| f.is_file());
+
     {
         let stdout = io::stdout();
         let handle = stdout.lock();
         let mut buffer = io::BufWriter::new(handle);
 
-        for path in config.paths().iter() {
-            writeln!(buffer, "{}", path.display())?;
+        for file in files {
+            writeln!(buffer, "{}: {:?}", file.display(), identify(file))?;
         }
     } // end scope to unlock stdout and flush
 
     Ok(())
+}
+
+#[must_use]
+pub fn identify(file: &PathBuf) -> Option<String> {
+    // TODO: identify_by_interpreter
+    identify_by_globs(file)
+}
+
+fn identify_by_globs(file: &PathBuf) -> Option<String> {
+    let rust = Pattern::new("*.rs").unwrap();
+
+    if rust.matches_path(file) {
+        Some("Rust".to_owned())
+    } else {
+        None
+    }
 }
